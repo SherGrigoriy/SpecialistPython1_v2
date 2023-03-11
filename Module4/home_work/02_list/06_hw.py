f202e80e79a4483b50e4bdf094a1eb21c2bf5fef
SherGrigoriy <125800297+SherGrigoriy@users.noreply.github.com>
2023-03-11 19:17:41 +0300

    Update 06_hw.py

--- Module4/home_work/02_list/06_hw.py
+++ Module4/home_work/02_list/06_hw.py
@@ -33,13 +33,46 @@ items = [
 ]
 # Найдите:
 print("Товары на складе представлены брэндами: ")
-
-# TODO: your code here
+summ_items = len(items)
+brands = []
+qty_brand = 0
+for unit in range(0, summ_items):
+    for element in items:
+        brands.insert(0, items[unit]["brand"])
+        break
+brands_no_dub = []
+for brand in brands:
+    if brand not in brands_no_dub:
+        brands_no_dub.append(brand)
+print(*brands_no_dub, sep=", ")
 
 print("На складе больше всего товаров брэнда(ов): ")
-
-# TODO: your code here
+summ_items = len(items)
+brands = []
+max_brand = items[0]["brand"]
+qty_brand = 0
+for unit in range(0, summ_items):
+    for element in items:
+        brands.insert(0, items[unit]["brand"])
+        break
+for brand in brands:
+    qty = brands.count(brand)
+    if qty > qty_brand:
+        qty_brand = qty
+        max_brand = brand
+print(max_brand)
 
 print("На складе самый дорогой товар брэнда(ов): ")
-
-# TODO: your code here
+summ_items = len(items)
+brands = []
+max_brand = items[0]["brand"]
+qty_brand = 0
+max_price = 0
+expens_brand = items[0]["brand"]
+for unit in range(0, summ_items):
+    for element in items:
+        if items[unit]["price"] > max_price:
+            max_price = items[unit]["price"]
+            expens_brand = items[unit]["brand"]
+        break
+print(expens_brand)
